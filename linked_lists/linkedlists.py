5aeecf474a677f5227f4557b4d174afff85cb94d
Evan Wondrasek <evan@evanw.com>
2014-06-25 22:09:23 -0500

    Initial commit of Linked Lists
    Updated .gitignore to ignore PyCharm directory

---
+++ linked_lists/linkedlists.py
@@ -0,0 +1,91 @@
+class Node:
+    """ Defines a Node object with a value and a Next pointer
+    """
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+class LinkedList:
+    """ Defines a Linked List object
+    """
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.count = 0
+
+    def add_first(self, node):
+        node.next = self.head
+        self.head = node
+
+        self.count += 1
+
+        if self.tail is None:
+            self.tail = self.head
+
+    def add_last(self, node):
+        if self.count == 0:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+
+        self.count += 1
+
+    def remove_first(self):
+        if self.count == 0:
+            return
+
+        self.head = self.head.next
+
+        self.count -= 1
+
+        if self.count == 0:
+            self.tail = None
+
+    def remove_last(self):
+        if self.count == 0:
+            return
+
+        if self.count == 1:
+            self.tail = None
+            self.head = None
+        else:
+            current = self.head
+
+            while current.next != self.tail:
+                current = current.next
+
+            self.tail = current
+
+        self.count -= 1
+
+    def remove(self, node):
+        pass
+
+    def find_node(self, node):
+        pass
+
+    def find_value(self, value):
+        pass
+
+    def enumerate(self):
+        pass
+
+    def deep_count(self):
+        counter = 0
+
+        current = self.head
+        while current is not None:
+            counter += 1
+            current = current.next
+
+        return counter
+
+
+def print_nodes(head):
+    while head is not None:
+        print("Node value: ", head.Value)
+        head = head.Next
---
+++ linked_lists/test_linked_lists.py
@@ -0,0 +1,162 @@
+from unittest import TestCase
+
+from linked_lists.linkedlists import Node
+from linked_lists.linkedlists import LinkedList
+
+class TestLinkedLists(TestCase):
+    def test_linked_list_add_one(self):
+        ll = LinkedList()
+
+        ll.add_first(Node(10, None))
+
+        self.assertEqual(1, ll.count)
+        self.assertIsNot(ll.head, None)
+        self.assertIsNot(ll.tail, None)
+        self.assertEqual(10, ll.head.value)
+
+    def test_linked_list_add_multiple(self):
+        ll = LinkedList()
+
+        ll.add_first(Node(14))
+        ll.add_first(Node(28))
+        ll.add_first(Node(52))
+
+        self.assertEqual(3, ll.count)
+        self.assertEqual(14, ll.tail.value)
+        self.assertEqual(52, ll.head.value)
+
+    def test_ll_add_two(self):
+        ll = LinkedList()
+
+        ll.add_first(Node(1))
+        ll.add_first(Node(2))
+
+        self.assertEqual(2, ll.count)
+        self.assertEqual(2, ll.head.value)
+        self.assertEqual(1, ll.tail.value)
+
+    def test_ll_empty_add_to_end_one(self):
+        ll = LinkedList()
+
+        ll.add_last(Node(1))
+
+        self.assertEqual(1, ll.count)
+        self.assertEqual(1, ll.head.value)
+        self.assertEqual(1, ll.tail.value)
+
+    def test_ll_add_to_end_two(self):
+        ll = LinkedList()
+
+        ll.add_last(Node(3))
+        ll.add_last(Node(115))
+
+        self.assertEqual(2, ll.count)
+        self.assertEqual(3, ll.head.value)
+        self.assertEqual(115, ll.tail.value)
+
+    def test_ll_add_to_end_three(self):
+        ll = LinkedList()
+
+        ll.add_last(Node(3))
+        ll.add_last(Node(115))
+        ll.add_last(Node(135))
+
+        self.assertEqual(3, ll.count)
+        self.assertEqual(3, ll.deep_count())
+        self.assertEqual(3, ll.head.value)
+        self.assertEqual(135, ll.tail.value)
+
+    def test_ll_three_remove_first(self):
+        ll = LinkedList()
+
+        ll.add_last(Node(3))
+        ll.add_last(Node(18))
+        ll.add_last(Node(4))
+
+        ll.remove_first()
+
+        self.assertEqual(2, ll.count)
+        self.assertEqual(2, ll.deep_count())
+        self.assertEqual(18, ll.head.value)
+        self.assertEqual(4, ll.tail.value)
+
+    def test_ll_two_remove_first(self):
+        ll = LinkedList()
+
+        ll.add_last(Node(18))
+        ll.add_last(Node(4))
+
+        ll.remove_first()
+
+        self.assertEqual(1, ll.count)
+        self.assertEqual(1, ll.deep_count())
+        self.assertEqual(4, ll.head.value)
+        self.assertEqual(4, ll.tail.value)
+
+    def test_ll_one_remove_first(self):
+        ll = LinkedList()
+
+        ll.add_first(Node(212))
+
+        ll.remove_first()
+
+        self.assertEqual(0, ll.count)
+        self.assertEqual(0, ll.deep_count())
+        self.assertIsNone(ll.head)
+        self.assertIsNone(ll.tail)
+
+    def test_ll_remove_first_no_items(self):
+        ll = LinkedList()
+
+        ll.remove_first()
+
+        self.assertEqual(0, ll.count)
+        self.assertEqual(0, ll.deep_count())
+        self.assertIsNone(ll.head)
+        self.assertIsNone(ll.tail)
+
+    def test_ll_remove_last_three_items(self):
+        ll = LinkedList()
+
+        ll.add_last(Node(10))
+        ll.add_last(Node(20))
+        ll.add_last(Node(30))
+
+        ll.remove_last()
+
+        self.assertEqual(2, ll.count)
+        self.assertEqual(10, ll.head.value)
+        self.assertEqual(20, ll.tail.value)
+
+    def test_ll_remove_last_two_items(self):
+        ll = LinkedList()
+
+        ll.add_last(Node(10))
+        ll.add_last(Node(20))
+
+        ll.remove_last()
+
+        self.assertEqual(1, ll.count)
+        self.assertEqual(10, ll.head.value)
+        self.assertEqual(10, ll.tail.value)
+
+    def test_ll_remove_last_one_items(self):
+        ll = LinkedList()
+
+        ll.add_last(Node(10))
+        ll.remove_last()
+
+        self.assertEqual(0, ll.count)
+        self.assertEqual(0, ll.deep_count())
+        self.assertIsNone(ll.head)
+        self.assertIsNone(ll.tail)
+
+    def test_ll_remove_last_no_items(self):
+        ll = LinkedList()
+
+        ll.remove_last()
+
+        self.assertEqual(0, ll.count)
+        self.assertEqual(0, ll.deep_count())
+        self.assertIsNone(ll.head)
+        self.assertIsNone(ll.tail)
